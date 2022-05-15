3402f223ee7e2aa3c87a16b4152faeb61b15bc20
nitin kumar gupta <nitinkumargupta65@gmail.com>
2022-05-16 00:43:33 +0530

    All sorting algorithm...

---
+++ src/main/java/com/hardQuestion/array/RemoveConsecutiveVowelsFromString.java
@@ -0,0 +1,54 @@
+package com.hardQuestion.array;
+
+/**
+ * Remove consecutive vowels from string
+ * Given a string s of lowercase letters, we need to remove consecutive vowels from the string
+ * Input: geeks for geeks
+ * Output: geks for geks
+ * <p>
+ * Input : your article is in queue
+ * Output : your article is in qu
+ */
+
+
+public class RemoveConsecutiveVowelsFromString {
+    static char[] vowels = new char[]{'a', 'e', 'i', 'o', 'u'};
+    //  static String input = "geeks for geeks";
+    static String input = "your article is in queue";
+    static String expectedOutput = "yor article is in qu";
+    static String output="";
+
+    public static void main(String[] args) {
+        boolean flag =false;
+        for (int i = 1; i < input.length(); i++) {
+            if(is_vow(input.charAt(i-1))  && is_vow(input.charAt(i)) ){
+                if(!flag)
+                    output+= String.valueOf(input.charAt(i-1));
+                flag=true;
+            }else{
+                if(!flag)
+                output+=String.valueOf(input.charAt(i-1));
+                flag=false;
+            }
+        }
+        System.out.println(output);
+    }
+
+    public static boolean isVowel(char c)
+    {
+        String vowels = "aeiouAEIOU";
+        for(int i=0; i<10; i++)
+            if(c==vowels.charAt(i))
+                return true;
+        return false;
+    }
+
+    static boolean is_vow(char c) {
+        // this compares vowel // with character 'c'
+        return (c == 'a') || (c == 'e') ||
+                (c == 'i') || (c == 'o') ||
+                (c == 'u');
+    }
+
+
+}
